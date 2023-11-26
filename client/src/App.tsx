c30bfa1743d957eefe546305328bf59a8165b345
sacden <sacden2009@gmail.com>
2023-11-26 15:31:08 +0100

    feat: add employee list structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,83 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import * as React from "react";
+
 import "./App.css";
+import profilePicture from "./assets/people/profile-picture-1.jpg";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <h1 className="font-bold underline text-red-800"> Hello world!</h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <div className="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-white dark:bg-gray-900">
+        <div></div>
+        <label htmlFor="table-search" className="sr-only">
+          Search
+        </label>
+        <div className="relative">
+          <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+            </svg>
+          </div>
+          <input
+            type="text"
+            id="table-search-users"
+            className="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search for users"
+          />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              Name
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Phone number
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Position
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Department
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Boss
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Status
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Action
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+            <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+              <img className="w-10 h-10 rounded-full" src={profilePicture} alt="Jese image" />
+              <div className="ps-3">
+                <div className="text-base font-semibold">Jack White</div>
+                <div className="font-normal text-gray-500">jack.white@example.com</div>
+              </div>
+            </th>
+            <td className="px-6 py-4">+1122334455</td>
+            <td className="px-6 py-4">Marketing Manager</td>
+            <td className="px-6 py-4">Marketing</td>
+            <td className="px-6 py-4">Laura Clark</td>
+            <td className="px-6 py-4">
+              <div className="flex items-center">
+                <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> Online
+              </div>
+            </td>
+            <td className="px-6 py-4">
+              <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                Edit user
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
 
