830898e0821681a27b30fc4b576d2e089f257d1d
sacden <sacden2009@gmail.com>
2023-11-26 16:35:15 +0100

    feat: add dynamic data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,19 @@
 import * as React from "react";
 
 import "./App.css";
-import profilePicture from "./assets/people/profile-picture-1.jpg";
+import { employees } from "./data/employees";
 
 function App() {
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
       <div className="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-white dark:bg-gray-900">
         <div></div>
-        <label htmlFor="table-search" className="sr-only">
-          Search
-        </label>
+        {/* <label htmlFor="table-search" className="sr-only"> */}
+        <label className="sr-only">Search</label>
         <div className="relative">
           <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
             <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
             </svg>
           </div>
           <input
@@ -28,53 +27,45 @@ function App() {
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" className="px-6 py-3">
-              Name
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Phone number
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Position
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Department
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Boss
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Status
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Action
-            </th>
+            {["Name", "Phone number", "Position", "Department", "Boss", "Status", "Action"].map((item) => {
+              return (
+                <th scope="col" className="px-6 py-3">
+                  {item}
+                </th>
+              );
+            })}
           </tr>
         </thead>
         <tbody>
-          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-            <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-              <img className="w-10 h-10 rounded-full" src={profilePicture} alt="Jese image" />
-              <div className="ps-3">
-                <div className="text-base font-semibold">Jack White</div>
-                <div className="font-normal text-gray-500">jack.white@example.com</div>
-              </div>
-            </th>
-            <td className="px-6 py-4">+1122334455</td>
-            <td className="px-6 py-4">Marketing Manager</td>
-            <td className="px-6 py-4">Marketing</td>
-            <td className="px-6 py-4">Laura Clark</td>
-            <td className="px-6 py-4">
-              <div className="flex items-center">
-                <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> Online
-              </div>
-            </td>
-            <td className="px-6 py-4">
-              <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                Edit user
-              </a>
-            </td>
-          </tr>
+          {employees.map((employee) => {
+            return (
+              <tr key={employee.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                  <img className="w-10 h-10 rounded-full" src={`${employee.profileImage}`} alt="Jese image" />
+                  <div className="ps-3">
+                    <div className="text-base font-semibold">
+                      {employee.firstName} {employee.lastName}
+                    </div>
+                    <div className="font-normal text-gray-500">{employee.email}</div>
+                  </div>
+                </th>
+                <td className="px-6 py-4">{employee.phoneNumber}</td>
+                <td className="px-6 py-4">{employee.position}</td>
+                <td className="px-6 py-4">{employee.department}</td>
+                <td className="px-6 py-4">{employee.boss}</td>
+                <td className="px-6 py-4">
+                  <div className="flex items-center">
+                    <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> {employee.status}
+                  </div>
+                </td>
+                <td className="px-6 py-4">
+                  <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                    Edit user
+                  </a>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
