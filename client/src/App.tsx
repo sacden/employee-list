857275f8c64fc21af0cddcd61c4d8c30e98396cb
sacden <sacden2009@gmail.com>
2023-11-26 18:25:33 +0100

    feat: add routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,31 @@
 import * as React from "react";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import { EmployeeEditForm } from "./components/EmployeeEditForm/EmployeeEditForm";
 import { EmployeeList } from "./components/EmployeeList.tsx/EmployeeList";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <EmployeeList />,
+  },
+  {
+    path: "/employees/:id/edit",
+    element: <EmployeeEditForm />,
+  },
+  // {
+  //   path: "/employees/:id",
+  //   element: <EmployeeDetail />,
+  // },
+
+  // {
+  //   path: "/employees/create",
+  //   element: <EmployeeCreateForm />,
+  // },
+]);
+
 function App() {
-  return <EmployeeList />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ client/src/components/EmployeeEditForm/EmployeeEditForm.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export const EmployeeEditForm = () => {
+  return <div>EmployeeEditForm</div>;
+};
